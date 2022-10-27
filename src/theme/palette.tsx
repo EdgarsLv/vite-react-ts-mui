967056e0b7eac36546a710b24f1872e54b9cb731
EdgarsLv <edgars0707@gmail.com>
2022-10-27 23:47:59 +0300

    palette modifications

--- src/theme/palette.tsx
+++ src/theme/palette.tsx
@@ -1,9 +1,10 @@
-import { alpha, PaletteOptions, PaletteColorOptions } from '@mui/material/styles';
+import { PaletteOptions, PaletteColorOptions } from '@mui/material/styles';
+import { green } from '@mui/material/colors';
 
 const PRIMARY: PaletteColorOptions = {
-  light: '#5BE584',
-  main: '#00AB55',
-  dark: '#007B55',
+  light: green[200],
+  main: green[500],
+  dark: green[800],
 };
 const SECONDARY: PaletteColorOptions = {
   light: '#84A9FF',
@@ -31,59 +32,30 @@ const ERROR: PaletteColorOptions = {
   dark: '#B72136',
 };
 
-const GREY = {
-  0: '#FFFFFF',
-  100: '#F9FAFB',
-  200: '#F4F6F8',
-  300: '#DFE3E8',
-  400: '#C4CDD5',
-  500: '#919EAB',
-  600: '#637381',
-  700: '#454F5B',
-  800: '#212B36',
-  900: '#161C24',
-  500_8: alpha('#919EAB', 0.08),
-  500_12: alpha('#919EAB', 0.12),
-  500_16: alpha('#919EAB', 0.16),
-  500_24: alpha('#919EAB', 0.24),
-  500_32: alpha('#919EAB', 0.32),
-  500_48: alpha('#919EAB', 0.48),
-  500_56: alpha('#919EAB', 0.56),
-  500_80: alpha('#919EAB', 0.8),
-};
-
-const COMMON = {
-  common: { black: '#000', white: '#fff' },
+const COMMON_LIGHT: PaletteOptions = {
   primary: { ...PRIMARY, contrastText: '#fff' },
   secondary: { ...SECONDARY, contrastText: '#fff' },
-  info: { ...INFO, contrastText: '#fff' },
-  success: { ...SUCCESS, contrastText: GREY[800] },
-  warning: { ...WARNING, contrastText: GREY[800] },
   error: { ...ERROR, contrastText: '#fff' },
-  grey: GREY,
-  divider: GREY[500_24],
-  action: {
-    hover: GREY[500_8],
-    selected: GREY[500_16],
-    disabled: GREY[500_80],
-  },
+  warning: { ...WARNING, contrastText: '#212B36' },
+  info: { ...INFO, contrastText: '#fff' },
+  success: { ...SUCCESS, contrastText: '#212B36' },
 };
 
 const palette = {
   light: {
-    ...COMMON,
+    ...COMMON_LIGHT,
     mode: 'light',
-    text: { primary: GREY[800], secondary: GREY[600] },
+    text: { primary: '#212B36', secondary: '#637381' },
     background: { paper: '#dadfde', default: '#dadfde' },
-    action: { active: GREY[600], ...COMMON.action },
+    action: { active: '#637381' },
   } as PaletteOptions,
 
   dark: {
-    ...COMMON,
+    // ...COMMON_LIGHT,
     mode: 'dark',
-    text: { primary: '#fff', secondary: GREY[500] },
-    background: { paper: ' #090909', default: '#0a0a0a' },
-    action: { active: GREY[500], ...COMMON.action },
+    text: { primary: '#f4bd46', secondary: '#f8cd72' },
+    background: { paper: ' #343744', default: '#2C2F3C' },
+    action: { active: '#919EAB' },
   } as PaletteOptions,
 };
 
